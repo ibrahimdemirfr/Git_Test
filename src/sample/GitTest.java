0e84bd27ff590c0e543f1b0ee745a805999cca14
ibrahimdemirfr <67127448+ibrahimdemirfr@users.noreply.github.com>
2020-06-19 18:58:22 +0300

    Update GitTest.java

--- src/sample/GitTest.java
+++ src/sample/GitTest.java
@@ -3,6 +3,7 @@ package sample;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git den MERHABA");
+        System.out.println("Gitub den MERHABA!");
 
     }
 }
